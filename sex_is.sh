04729e3b520cda534bb6fd12acb9c28230febe32
bashprankster <60488917+bashprankster@users.noreply.github.com>
2020-02-07 14:50:24 +0000

    IS version of sex.sh

---
+++ sex_is.sh
@@ -0,0 +1,31 @@
+faster=("Hefjum leikana!" "Guð minn góður!" "Ekki þetta!" "Loksins!" "Fyrirheitna landið!" "Er þetta allt og sumt?" "Fokk, löggan!" "Mig hefði aldrei órað fyrir því" "Þú missir allt álit á mér ef ég geri það!" "Núna!" "Opnastu Sesam!" "Aftur!" "Fastar!" "Hraðar!" "Hjálp!" "Ríddu mér fastar!" "Er hann kominn inn?" "Þú ert ekki pabbi minn!" "Læknir, þetta er ekki öxlin mí.." "Nei nei nei, gerðu gullfiskinn!" "Hólý mólý!" "Hann er dauður, hann er dauður!" "Taktu mig, Róbert!" "Ég er Miðflokksmaður!" "Settu fjóra fingur inn!" "Þvílíkur elskhugi!" "Segðu eitthvað æsandi, melurinn þinn!" "Það þarf að mála loftið hérna" "Sjúgðu fastar!" "Dýrin heyra í okkur!" "Ekki á almannafæri!")
+said=("bölvaði" "skrækti" "krúnkaði" "urraði" "másaði" "stundi" "rýtti" "hló" "stamaði" "geispaði" "gall" "gaspraði" "rumdi" "blés" "hvíslaði" "vældi" "slefaði" "sagði" "grét" "öskraði" "kallaði" "flautaði")
+fadj=("heita" "fengilega" "óheppilega" "lostaóða" "níu-ára" "trukkalessulega" "tvíkynhneigða" "funheita" "sæta" "brókasóttar" "mjaðmastóra" "freknótta" "fjörutíuogfimm-ára" "hvíthærða" "stórbeinótta" "dýrðlega" "blinda" "skeggjaða" "bláeygða" "tungustóra" "vinalega" "píanóspilandi" "eyrnasleikjandi" "beljueygða" "sokkasniffandi" "lessulega" "háruga" "kynkalda")
+female=("farangursgeymslan" "druslan" "konan" "hertogaynjan" "þykjustukonan" "tussan" "hreina meyin" "leðurdruslan" "drottningin" "fyrrverandi nunnan" "tvíkynhneigða fínkan" "klappstýran" "skrifstofublókin" "litla eldspýtustúlkan" "illkvendið" "dóttir yfirmannsins" "aðstoðarkona þingmannsins" "amman" "mörgæsin" "tíkin" "flugfreyjan" "gengilbeinan" "mellan" "húsmóðirin" "vændiskonan" "kisan" "fyrrverandi konan")
+madjec=("þrýstni" "slefandi" "óseðjandi" "villti" "djöfladýrkandi" "feiti" "nefsnyrtandi" "magastrjúkandi" "blauti" "munnóði" "ljóti" "vel vaxni" "myndarlegi" "heilalausi" "typpastóri" "þrífætti" "kvenfataklæddi" "hamstraríðandi" "rassaríðandi" "hringvöðvasleikjandi" "skapaháratyggjandi" "kálkafandi" "kræklingssjúgandi" "eggjasleikjandi" "hjólreiðasætasniffandi")
+male=("auðkýfingurinn" "spassinn" "sígraði táningurinn" "erkihertoginn" "dvergurinn" "verktakinn" "stóri daninn" "jálkurinn" "asninn" "rafmagnaði állinn" "lamaði hasshausinn" "gamli perrinn" "hommabrytinn" "presturinn" "feministinn" "pervertinn" "kjúklingakáfarinn" "samkynhneigði flamíngóinn" "fyrrverandi skírlífismaðurinn" "eiturlyfjafíkillinn" "fyrrverandi karlmaðurinn" "byggingamaðurinn" "hárgreiðslumaðurinn" "tannlæknirinn" "dómarinn" "félagsfræðingurinn" "sálfræðingurinn" "kennarinn" "þingmaðurinn" "ráðherrann")
+diddled=("átti við" "lék við" "strauk" "umlukti" "sleikti" "sló" "sneri uppá" "herti" "gjöreyðilagði" "vökvaði" "tróð sér í gegnum" "hamraði" "beit" "saug" "dúndraði í" "nuddaði sér upp við" "rúnkaði sér á" "slefaði á" "brundaði yfir")
+titadj=("hvítu" "bleikoddóttu" "rjómakenndu" "röku" "þrýstnu" "safaríku" "þungu" "risastóru" "mjúku" "skjálfandi" "rósakenndu" "kringlulaga" "æðakenndu" "skoppandi" "blóðugu" "hallandi" "lekandi" "hörðu" "ruggandi" "vöðvastæltu")
+knockers=("kringlurnar" "melónunar" "hrúgurnar" "blómin" "tútturnar" "brjóstin" "kýlin" "fjársjóðina" "bollurnar" "tappana" "súlurnar" "handakrikana" "skiltin" "olnbogana" "augun" "kókoshneturnar" "krukkurnar" "lungun" "framljósin" "geisladrifin" "stuðarana" "hnén" "spældu eggin" "rasskinnarnar" "kellurnar" "eyrnasneplana" "basúkkurnar" "mjólkurkranana")
+thrust=("smellti" "þrýsti" "tróð" "lamdi" "keyrði" "renndi" "hamraði" "ýtti" "henti" "barði" "grýtti" "barði" "dýfði" "lét" "mátaði" "blessaði" "kom" "negldi" "neyddi" "skrúfaði")
+dongadj=("springandi" "framlengda" "glitrandi" "framúrskarandi" "fjólubláa" "sjóðheita" "bólgna" "harða" "keyrandi" "vörtukennda" "sjóðandi" "troðfulla" "trukkalega" "froðufellandi" "spírandi" "svínslega" "gervi" "bláæðakennda" "stækkaða" "hrossakennda" "púlserandi" "risavaxna" "holusundrandi" "snákakennda" "bogna" "stálslegna" "brynvarða" "hringlaga" "betrumbætta" "járnaða" "særða" "snarminnkandi" "bólgnandi" "smávægilega" "beinótta")
+dong=("óboðna gestinum" "prikinu" "stubbnum" "meðlimnum" "kjötinu" "hátigninni" "boganum" "sporðdrekanum" "hamrinum" "þorsknum" "atgeirnum" "snáknum" "potaranum" "tannstönglinum" "gleðipinnanum" "klósettburstanum" "tólinu" "manndómnum" "sleikjóinum" "nýrnapotaranum" "kertinu" "naglanum" "pungnum" "fingrinum" "fætinum" "tungubroddinum" "eineygða undraorminum" "drjólanum" "göndlinum" "banananum" "miðfætinum" "hálsklútnum" "þyrluspaðanum" "drjólanum")
+twatadj=("sláandi" "hungruðu" "rennblautu" "galopnu" "slefandi" "gestrisnu" "troðfullu" "hámandi" "ryðguðu" "banhungruðu" "smjattandi" "glitrandi" "hriplekandi" "hrúðurkenndu" "götuðu" "klístruðu" "bleiku" "rykföllnu" "þröngu" "illa lyktandi" "blautu" "lauslátu" "öru" "hjálparvana" "bananatroðnu" "illa förnu" "nörtuðu" "þéttu" "margnotuðu" "þakklátu" "mygluðu")
+twat=("mýrina" "hunangskrukkuna" "sultukrukkuna" "smjördolluna" "kirsuberjakökuna" "raufina" "holuna" "bleytuna" "hellisraufina" "skeggjuðu ostruna" "spilakassaraufina" "höllina" "rassaholuna" "augnholuna" "mýktina" "opnunina" "naflagatið" "munnholuna" "nösina" "kuntuna")
+
+selected_faster=${faster[$RANDOM % ${#faster[@]} ]}
+selected_said=${said[$RANDOM % ${#said[@]} ]}
+selected_fadj=${fadj[$RANDOM % ${#fadj[@]} ]}
+selected_female=${female[$RANDOM % ${#female[@]} ]}
+selected_madjec=${madjec[$RANDOM % ${#madjec[@]} ]}
+selected_male=${male[$RANDOM % ${#male[@]} ]}
+selected_diddled=${diddled[$RANDOM % ${#diddled[@]} ]}
+selected_titadj=${titadj[$RANDOM % ${#titadj[@]} ]}
+selected_knockers=${knockers[$RANDOM % ${#knockers[@]} ]}
+selected_thrust=${thrust[$RANDOM % ${#thrust[@]} ]}
+selected_dongadj=${dongadj[$RANDOM % ${#dongadj[@]} ]}
+selected_dong=${dong[$RANDOM % ${#dong[@]} ]}
+selected_twatadj=${twatadj[$RANDOM % ${#twatadj[@]} ]}
+selected_twat=${twat[$RANDOM % ${#twat[@]} ]}
+
+echo "\"$selected_faster\" $selected_said $selected_fadj $selected_female meðan $selected_madjec $selected_male $selected_diddled $selected_titadj $selected_knockers og $selected_thrust $selected_dongadj $selected_dong í $selected_twatadj $selected_twat á henni."
